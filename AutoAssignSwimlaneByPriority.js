4f269bbc4b5534dfb14878f438588ebde1d09404
zsilliman <zsilliman@users.noreply.github.com>
2022-02-14 11:46:45 -0500

    Create AutoAssignSwimlaneByPriority.js

---
+++ AutoAssignSwimlaneByPriority.js
@@ -0,0 +1,25 @@
+KT.tasks.groupUpdateFilters.push({
+  name: 'AutoAssignSwimlaneByPriority',
+  action: function(tasks, attrs) {
+    var sl; var slName;
+    if (attrs && attrs['swimlane_id'] && _.filter(tasks, function(t) {
+      return t.get('swimlane_id') === attrs['swimlane_id'];
+    }).length === 0) {
+      if (sl = KT.Swimlane.get(attrs['swimlane_id'])) {
+        slName = (sl.get('priority') || '').toLowerCase();
+        console.log(s1Name);
+        var priorities = ['low', 'medium', 'high'];
+        if (slName.indexOf('low') >= 0) {
+          return attrs.swimlane_id = 'Low Priority';
+        }
+        else if (slName.indexOf('medium') >= 0) {
+          return attrs.swimlane_id = 'Medium Priority';
+        }
+        else if (slName.indexOf('high') >= 0) {
+          console.log("High prior");
+          return attrs.swimlane_id = 'High Priority';
+        }
+      }
+    }
+  }
+});
